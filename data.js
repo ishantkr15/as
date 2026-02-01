38c2445079390278da782ba12bbc54037895a510
ishantkr15 <i8534848k@gmail.com>
2026-02-01 22:01:49 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,180 +1,198 @@
-window.lastUpdated = "1/30/2026, 3:49:38 PM";
+window.lastUpdated = "2/1/2026, 10:00:52 PM";
 window.appsData = [
-    {
-        "id": "Career Will",
-        "name": "Career Will",
-        "developer": "ASMULTIVERSE",
-        "version": "14.21.06",
-        "size": "19MB",
-        "downloads": "90K",
-        "tags": [
-            ""
-        ],
-        "iconUrl": "https://i.postimg.cc/D0ghh7s2/IMG-20250927-222606.jpg",
-        "downloadUrl": "https://lksfy.com/Careerwill123",
-        "description": "",
-        "seoTitle": "",
-        "seoKeywords": "",
-        "seoDesc": ""
-    },
-    {
-        "id": "Khan Global Studies",
-        "name": "Khan Global Studies",
-        "developer": "ASMULTIVERSE",
-        "version": "22.21.06",
-        "size": "27 MB",
-        "downloads": "100k",
-        "tags": [
-            "New"
-        ],
-        "iconUrl": "https://d2bps9p1kiy4ka.cloudfront.net/5b09189f7285894d9130ccd0/67a2a82f-5eca-4804-9d47-69f05e3831d1.png",
-        "downloadUrl": "https://lksfy.com/khansir",
-        "description": "Best Teacher | Free Education | Live Classes | DPP | Notes",
-        "seoTitle": "",
-        "seoKeywords": "",
-        "seoDesc": ""
-    },
-    {
-        "id": "vidyakul",
-        "name": "VidyaKul",
-        "developer": "VK MOD APK v10.21.6",
-        "version": "v10.21.6",
-        "size": "21.48 MB",
-        "downloads": "10,000+",
-        "tags": [
-            "Educational",
-            "New"
-        ],
-        "iconUrl": "https://i.postimg.cc/Vkm2545X/20250929-160150.jpg",
-        "downloadUrl": "https://lksfy.com/vidhyakul",
-        "description": "VidyaKul - The Learning App for Class 9th to 12th, JEE, NEET, AIIMS, and other competitive exams.",
-        "seoTitle": "",
-        "seoKeywords": "",
-        "seoDesc": ""
-    },
-    {
-        "id": "kd-live",
-        "name": "KD LIVE [KD]",
-        "developer": "KD MOD APK - v10.21.6",
-        "version": "v10.21.6",
-        "size": "21.78 MB",
-        "downloads": "10,000+",
-        "tags": [
-            "Educational",
-            "New"
-        ],
-        "iconUrl": "https://i.postimg.cc/65HVCbLY/20250923-192017.jpg",
-        "downloadUrl": "https://lksfy.com/kd",
-        "description": "KD Live App for SSC, BANK, RAILWAY, POLICE, NDA, CDS, AIRFORCE, NAVY and other competitive exams.",
-        "seoTitle": "",
-        "seoKeywords": "",
-        "seoDesc": ""
-    },
-    {
-        "id": "education-baba",
-        "name": "Education Baba",
-        "developer": "EB MOD APK v10.21.6",
-        "version": "v10.21.6",
-        "size": "21.78 MB",
-        "downloads": "10,000+",
-        "tags": [
-            "Educational",
-            "New"
-        ],
-        "iconUrl": "https://i.ibb.co/9kSMLvKQ/x.jpg",
-        "downloadUrl": "https://lksfy.com/baba",
-        "description": "Best App for Bihar Board Student Class 11th & 12th.",
-        "seoTitle": "",
-        "seoKeywords": "",
-        "seoDesc": ""
-    },
-    {
-        "id": "rojgar-with-ankit",
-        "name": "Rojgar With Ankit [RWA]",
-        "developer": "RWA MOD APK - v10.21.6",
-        "version": "v10.21.6",
-        "size": "25.18 MB",
-        "downloads": "30,000+",
-        "tags": [
-            "Educational"
-        ],
-        "iconUrl": "https://i.postimg.cc/cLytYbpL/20231229-165646.jpg",
-        "downloadUrl": "https://lksfy.com/rwa",
-        "description": "Rojgar With Ankit is the most trusted app for competitive exams preparation.",
-        "seoTitle": "",
-        "seoKeywords": "",
-        "seoDesc": ""
-    },
-    {
-        "id": "selection-way",
-        "name": "Selection Way",
-        "developer": "SW MOD APK v11.21.6",
-        "version": "v11.21.6",
-        "size": "22.78 MB",
-        "downloads": "30,000+",
-        "tags": [
-            "Educational",
-            "Popular"
-        ],
-        "iconUrl": "https://i.ibb.co/60cF3WS2/x.jpg",
-        "downloadUrl": "https://lksfy.com/selectionway",
-        "description": "Selection Way is an online learning platform for various competitive exams.",
-        "seoTitle": "",
-        "seoKeywords": "",
-        "seoDesc": ""
-    },
-    {
-        "id": "study-iq",
-        "name": "Study IQ",
-        "developer": "SIQ MOD APK v2.21.6",
-        "version": "v2.21.6",
-        "size": "21.94 MB",
-        "downloads": "10,000+",
-        "tags": [
-            "Educational",
-            "New"
-        ],
-        "iconUrl": "https://i.ibb.co/qLdK3Q1d/x.jpg",
-        "downloadUrl": "https://lksfy.com/study",
-        "description": "Study IQ Education is the most loved education platform in India.",
-        "seoTitle": "",
-        "seoKeywords": "",
-        "seoDesc": ""
-    },
-    {
-        "id": "as-multiverse",
-        "name": "AS MULTIVERSE [PW]",
-        "developer": "PW MOD APK v5.02.7",
-        "version": "v5.02.7",
-        "size": "24.96 MB",
-        "downloads": "1,00,000+",
-        "tags": [
-            "Educational",
-            "Popular"
-        ],
-        "iconUrl": "https://d2bps9p1kiy4ka.cloudfront.net/5b09189f7285894d9130ccd0/e00a5c58-461c-4c43-a911-d606e00ad53b.png",
-        "downloadUrl": "https://lksfy.com/pwmod",
-        "description": "Physics Wallah - Alakh Pandey is an online education platform.",
-        "seoTitle": "",
-        "seoKeywords": "",
-        "seoDesc": ""
-    },
-    {
-        "id": "next-toppers",
-        "name": "Next Toppers",
-        "developer": "NT MOD APK v14.21.6",
-        "version": "v15.21.6",
-        "size": "21.27 MB",
-        "downloads": "30,000+",
-        "tags": [
-            "Educational",
-            "Popular"
-        ],
-        "iconUrl": "https://i.postimg.cc/W3v5fRYr/square-icon.png",
-        "downloadUrl": "https://lksfy.com/next",
-        "description": "Next Toppers is a learning app for students.",
-        "seoTitle": "NExt toppers mod apk",
-        "seoKeywords": "",
-        "seoDesc": ""
-    }
+  {
+    "id": "apna college  mod apk ",
+    "name": "Apna college  Mod Apk ",
+    "developer": "MadXAbhi",
+    "version": "v1.21.6",
+    "size": "21 MB",
+    "downloads": "50000+",
+    "tags": [
+      "Educational",
+      "New"
+    ],
+    "iconUrl": "https://i.ibb.co/0y74Ks8X/x.jpg",
+    "downloadUrl": "https://mega.nz/file/io4miYRa#eZXAw6FcR_7ELMb5PIbQPdLtZHI78CmqZ9QPqUx755M",
+    "description": "Get all Apna College paid courses for free! Download the latest modded APK for 2026 with unlocked Delta 3.0, DSA, Java, & Web Development courses. No subscription required. Install guide included.\n\nFeatures List:\n\nAll Delta 3.0 courses unlocked (Java, DSA, Web Dev, etc.)\n\nNo subscription or login required.\n\nDownload videos for offline viewing.\n\nAll premium features activated.\n\nAds removed from the app.",
+    "seoTitle": "Apna College All Courses Free 2026 | Download Mod APK (Unlocked Premium)",
+    "seoKeywords": "apna college mod apk, apna college premium unlocked apk, apna college all courses free download, apna college delta 3.0 mod, apna college paid courses free apk, apna college cracked app 2024, download apna college premium for free, apna college pro version apk, get apna college course without payment, apna college hack apk latest version, apna college unlocked courses, free premium courses apna college, apna college modded app, apna college cracked version, apna college premium courses free",
+    "seoDesc": "Get all Apna College paid courses for free! Download the latest modded APK for 2024 with unlocked Delta 3.0, DSA, Java, & Web Development courses. No subscription required. Install guide included."
+  },
+  {
+    "id": "Career Will",
+    "name": "Career Will",
+    "developer": "ASMULTIVERSE",
+    "version": "14.21.06",
+    "size": "19MB",
+    "downloads": "90K",
+    "tags": [
+      ""
+    ],
+    "iconUrl": "https://i.postimg.cc/D0ghh7s2/IMG-20250927-222606.jpg",
+    "downloadUrl": "https://lksfy.com/Careerwill123",
+    "description": "",
+    "seoTitle": "",
+    "seoKeywords": "",
+    "seoDesc": ""
+  },
+  {
+    "id": "Khan Global Studies",
+    "name": "Khan Global Studies",
+    "developer": "ASMULTIVERSE",
+    "version": "22.21.06",
+    "size": "27 MB",
+    "downloads": "100k",
+    "tags": [
+      "New"
+    ],
+    "iconUrl": "https://d2bps9p1kiy4ka.cloudfront.net/5b09189f7285894d9130ccd0/67a2a82f-5eca-4804-9d47-69f05e3831d1.png",
+    "downloadUrl": "https://lksfy.com/khansir",
+    "description": "Best Teacher | Free Education | Live Classes | DPP | Notes",
+    "seoTitle": "",
+    "seoKeywords": "",
+    "seoDesc": ""
+  },
+  {
+    "id": "vidyakul",
+    "name": "VidyaKul",
+    "developer": "VK MOD APK v10.21.6",
+    "version": "v10.21.6",
+    "size": "21.48 MB",
+    "downloads": "10,000+",
+    "tags": [
+      "Educational",
+      "New"
+    ],
+    "iconUrl": "https://i.postimg.cc/Vkm2545X/20250929-160150.jpg",
+    "downloadUrl": "https://lksfy.com/vidhyakul",
+    "description": "VidyaKul - The Learning App for Class 9th to 12th, JEE, NEET, AIIMS, and other competitive exams.",
+    "seoTitle": "",
+    "seoKeywords": "",
+    "seoDesc": ""
+  },
+  {
+    "id": "kd-live",
+    "name": "KD LIVE [KD]",
+    "developer": "KD MOD APK - v10.21.6",
+    "version": "v10.21.6",
+    "size": "21.78 MB",
+    "downloads": "10,000+",
+    "tags": [
+      "Educational",
+      "New"
+    ],
+    "iconUrl": "https://i.postimg.cc/65HVCbLY/20250923-192017.jpg",
+    "downloadUrl": "https://lksfy.com/kd",
+    "description": "KD Live App for SSC, BANK, RAILWAY, POLICE, NDA, CDS, AIRFORCE, NAVY and other competitive exams.",
+    "seoTitle": "",
+    "seoKeywords": "",
+    "seoDesc": ""
+  },
+  {
+    "id": "education-baba",
+    "name": "Education Baba",
+    "developer": "EB MOD APK v10.21.6",
+    "version": "v10.21.6",
+    "size": "21.78 MB",
+    "downloads": "10,000+",
+    "tags": [
+      "Educational",
+      "New"
+    ],
+    "iconUrl": "https://i.ibb.co/9kSMLvKQ/x.jpg",
+    "downloadUrl": "https://lksfy.com/baba",
+    "description": "Best App for Bihar Board Student Class 11th & 12th.",
+    "seoTitle": "",
+    "seoKeywords": "",
+    "seoDesc": ""
+  },
+  {
+    "id": "rojgar-with-ankit",
+    "name": "Rojgar With Ankit [RWA]",
+    "developer": "RWA MOD APK - v10.21.6",
+    "version": "v10.21.6",
+    "size": "25.18 MB",
+    "downloads": "30,000+",
+    "tags": [
+      "Educational"
+    ],
+    "iconUrl": "https://i.postimg.cc/cLytYbpL/20231229-165646.jpg",
+    "downloadUrl": "https://lksfy.com/rwa",
+    "description": "Rojgar With Ankit is the most trusted app for competitive exams preparation.",
+    "seoTitle": "",
+    "seoKeywords": "",
+    "seoDesc": ""
+  },
+  {
+    "id": "selection-way",
+    "name": "Selection Way",
+    "developer": "SW MOD APK v11.21.6",
+    "version": "v11.21.6",
+    "size": "22.78 MB",
+    "downloads": "30,000+",
+    "tags": [
+      "Educational",
+      "Popular"
+    ],
+    "iconUrl": "https://i.ibb.co/60cF3WS2/x.jpg",
+    "downloadUrl": "https://lksfy.com/selectionway",
+    "description": "Selection Way is an online learning platform for various competitive exams.",
+    "seoTitle": "",
+    "seoKeywords": "",
+    "seoDesc": ""
+  },
+  {
+    "id": "study-iq",
+    "name": "Study IQ",
+    "developer": "SIQ MOD APK v2.21.6",
+    "version": "v2.21.6",
+    "size": "21.94 MB",
+    "downloads": "10,000+",
+    "tags": [
+      "Educational",
+      "New"
+    ],
+    "iconUrl": "https://i.ibb.co/qLdK3Q1d/x.jpg",
+    "downloadUrl": "https://lksfy.com/study",
+    "description": "Study IQ Education is the most loved education platform in India.",
+    "seoTitle": "",
+    "seoKeywords": "",
+    "seoDesc": ""
+  },
+  {
+    "id": "as-multiverse",
+    "name": "AS MULTIVERSE [PW]",
+    "developer": "PW MOD APK v5.02.7",
+    "version": "v5.02.7",
+    "size": "24.96 MB",
+    "downloads": "1,00,000+",
+    "tags": [
+      "Educational",
+      "Popular"
+    ],
+    "iconUrl": "https://d2bps9p1kiy4ka.cloudfront.net/5b09189f7285894d9130ccd0/e00a5c58-461c-4c43-a911-d606e00ad53b.png",
+    "downloadUrl": "https://lksfy.com/pwmod",
+    "description": "Physics Wallah - Alakh Pandey is an online education platform.",
+    "seoTitle": "",
+    "seoKeywords": "",
+    "seoDesc": ""
+  },
+  {
+    "id": "next-toppers",
+    "name": "Next Toppers",
+    "developer": "NT MOD APK v14.21.6",
+    "version": "v15.21.6",
+    "size": "21.27 MB",
+    "downloads": "30,000+",
+    "tags": [
+      "Educational",
+      "Popular"
+    ],
+    "iconUrl": "https://i.postimg.cc/W3v5fRYr/square-icon.png",
+    "downloadUrl": "https://lksfy.com/next",
+    "description": "Next Toppers is a learning app for students.",
+    "seoTitle": "NExt toppers mod apk",
+    "seoKeywords": "",
+    "seoDesc": ""
+  }
 ];
